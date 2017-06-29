9abec9cc66dd0241b33d8a2541d480cf10f67884
fengweiyuan <107639177@qq.com>
2017-06-29 20:57:27 +0800

    first commit

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -3,7 +3,7 @@ This module collects helper functions and classes that "span" multiple levels
 of MVC. In other words, these functions/classes introduce controlled coupling
 for convenience's sake.
 """
-
+# fwy
 import warnings
 
 from django.core import urlresolvers
